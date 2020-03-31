5a865d4dee4fdefba7d837d34049a9b71a46c5e1
Essam <essamabuissa@hotmail.com>
2020-03-31 22:54:48 +0300

    Export SET_MESSAGES

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -3,3 +3,4 @@ export const SET_ERRORS = "SET_ERRORS";
 export const POST_FORM = "POST_FORM";
 export const ADD_CHANNEL = "ADD_CHANNEL";
 export const SET_CHANNEL = "SET_CHANNEL";
+export const SET_MESSAGES = "SET_MESSAGES";
