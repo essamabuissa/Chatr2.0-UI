a797b387b7e973657a64a30271c3690e9cafea68
Essam <essamabuissa@hotmail.com>
2020-04-02 01:49:12 +0300

    Trying to dispatch loading

--- src/redux/actions/messages.js
+++ src/redux/actions/messages.js
@@ -1,11 +1,13 @@
 import instance from "./instance";
-import { SET_MESSAGES, ADD_MESSAGE } from "./actionTypes";
+import { SET_MESSAGES, ADD_MESSAGE, SET_LOADING } from "./actionTypes";
 
 export const fetchMessages = channelID => async dispatch => {
   try {
     // dispatch({ type: SET_MESSAGES, payload: [] });
     const response = await instance.get(`/channels/${channelID}/`);
+    console.log(response);
     const messages = response.data;
+    dispatch({ type: SET_LOADING });
     dispatch({ type: SET_MESSAGES, payload: messages.reverse() });
   } catch {
     console.error("error at fetching message");
