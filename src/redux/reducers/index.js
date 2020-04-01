7ebf9a3ccec58671f936d0182bea0c7ae755eb6a
Essam <essamabuissa@hotmail.com>
2020-04-02 01:50:56 +0300

    Combine loading reducer to the previous reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,10 +5,12 @@ import userReducer from "./user";
 import errorReducer from "./errors";
 import channelsReducer from "./channels";
 import messagesReducer from "./messages";
+import loadingReducer from "./loading";
 
 export default combineReducers({
   messages: messagesReducer,
   channels: channelsReducer,
   user: userReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  loading: loadingReducer
 });
